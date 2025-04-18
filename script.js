5004bb5d611d2aaf735407cf84480de6ba8eeef6
Dmukherjeetextiles <dmukherjeetextiles@gmail.com>
2025-04-19 02:30:53 +0530

    dynamic background

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let lastTime = null; 
 const HUE_CHANGE_RATE = 0.025; 
 
+document.getElementById("capitalD").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+
+document.getElementById("capitalE").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+document.getElementById("capitalB").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+document.getElementById("capitalR").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+
+document.getElementById("capitalU").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+document.getElementById("capitalP").addEventListener("click", function() {
+    window.location.href = "resume.pdf";
+  });
+
+
+// --- Function to update the hue variable ---
 function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime;
